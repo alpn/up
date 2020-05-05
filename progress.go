aaa280cd8f6060ebb28e409962eeb07b8bf4ac78
alpn <alpn@users.noreply.github.com>
2020-05-05 14:33:56 -0400

    add progress bar
    	new file:   progress.go
    	modified:   upload.go

---
+++ progress.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/vbauerster/mpb"
+	"github.com/vbauerster/mpb/decor"
+)
+
+const total = 100
+
+func updateBar(bar *mpb.Bar, name string) {
+
+	stat := B2Client.Status().Writers[name]
+	if stat != nil {
+		prog := stat.Progress
+		var sum float64
+		for _, p := range prog {
+			sum += p
+		}
+		bar.SetTotal(int64(total*len(prog)), false)
+		bar.SetCurrent(int64(total*sum) + 1)
+	}
+}
+
+func showProgress(stop chan bool, wg *sync.WaitGroup, bucketName string, name string) {
+
+	writerName := fmt.Sprintf("%s/%s", bucketName, name)
+	p := mpb.New(mpb.WithWidth(64))
+
+	bar := p.AddBar(int64(total),
+		mpb.PrependDecorators(
+
+			// display name with one space on the right
+			decor.Name(name, decor.WC{W: len(name) + 1, C: decor.DidentRight}),
+
+			// replace ETA decorator with "done" message, OnComplete event
+			decor.OnComplete(
+				decor.AverageETA(decor.ET_STYLE_GO, decor.WC{W: 4}), "done",
+			),
+		),
+		mpb.AppendDecorators(decor.Percentage()),
+	)
+
+	defer func() {
+		bar.SetTotal(1, true)
+		bar.SetCurrent(1)
+		p.Wait()
+		wg.Done()
+	}()
+
+	for {
+
+		updateBar(bar, writerName)
+
+		timeoutchan := make(chan bool)
+		go func() {
+			<-time.After(time.Millisecond * 25)
+			timeoutchan <- true
+		}()
+
+		select {
+		case <-timeoutchan:
+		case <-stop:
+			return
+		}
+	}
+}
--- upload.go
+++ upload.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
+	"sync"
 
 	"github.com/kurin/blazer/b2"
 )
@@ -13,9 +13,20 @@ func uploadOneReader(ctx context.Context, bucket *b2.Bucket, src io.Reader, dstN
 	obj := bucket.Object(dstName)
 	writer := obj.NewWriter(ctx)
 
+	stop := make(chan bool)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	defer func() {
+		stop <- true
+		wg.Wait()
+	}()
+
+	go showProgress(stop, &wg, bucket.Name(), dstName)
+
 	if _, err := io.Copy(writer, src); nil != err {
 		writer.Close()
 		return err
 	}
+
 	return writer.Close()
 }
