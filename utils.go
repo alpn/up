59d0f052709dbf3b202b02f0e2921b93d83e7741
alpn <alpn@users.noreply.github.com>
2020-07-07 09:44:01 -0400

    add ByteCountSI() to improve file size formatting
    	modified:   utils.go

--- utils.go
+++ utils.go
@@ -122,3 +122,18 @@ func pickBucketPrompt(buckets []*b2.Bucket, name string) (*b2.Bucket, error) {
 	}
 	return bucket, nil
 }
+
+// ByteCountSI - credit: https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
